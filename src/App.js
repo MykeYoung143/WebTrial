27585465de1767e8c16375c2d4043e7094f3faa5
unknown <michaelalex997@gmail.com>
2021-10-26 09:51:48 +0700

    Saving Files into the github

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <div>
       <Switch>
-        <Route exact path='/' component={Login}/>
+        <Route path='/' component={Login}/>
       </Switch>
     </div>
   );
